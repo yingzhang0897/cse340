0a4a90cdea355ddbdedbfb5a09501d70b649d77e
“yingzhang0897” <yinganticpc@gmail.com>
2025-01-16 10:49:15 -0800

    add query 4 and query 6 at bottom

--- database/db-rebuild.sql
+++ database/db-rebuild.sql
@@ -239,3 +239,15 @@ VALUES   (
     'White',
     5
   );
+
+--query 4: Description Update
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id= 10;
+
+--query 6: image path update
+UPDATE public.inventory
+SET 
+	inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
+
