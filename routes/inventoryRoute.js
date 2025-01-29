ffabb4803fd55c8002b3227fbad627eff87cc977
“yingzhang0897” <yinganticpc@gmail.com>
2025-01-29 10:42:53 -0800

    build classification view

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = new express.Router() 
+const invController = require("../controllers/invController")
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 
@@ -27,6 +28,9 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 //Index route
 app.get('/', utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
