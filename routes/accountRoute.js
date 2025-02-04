e455a9a412d73e62e336f09eed2d1b93bb144564
“yingzhang0897” <yinganticpc@gmail.com>
2025-02-03 21:51:56 -0800

    finish server side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,10 +2,14 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
 router.get("/login",utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 //process registration data
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+router.post("/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
