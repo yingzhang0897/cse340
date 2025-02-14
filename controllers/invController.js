8563c3ed7f65d7ad15fba8e43fc3242f30578184
“yingzhang0897” <yinganticpc@gmail.com>
2025-02-13 21:37:29 -0800

    delete inventory finished

--- controllers/invController.js
+++ controllers/invController.js
@@ -236,19 +236,17 @@ invCont.updateInventory = async function (req, res, next) {
 invCont.buildDeleteInventory = async function (req, res, next) {
   const inv_id = parseInt(req.params.invId)
   let nav = await utilities.getNav()
-  const itemData = await invModel.getInventoryById(inv_id)
-  const classificationSelect = await utilities.buildClassificationList(itemData[0].classification_id)
-  const itemName = `${itemData[0].inv_make} ${itemData[0].inv_model}`
-  res.render("./inventory/delete-inventory", {
+  const itemData = await invModel.getVehicleById(inv_id)
+  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
+  res.render("inventory/delete-confirm", {
     title: "Delete " + itemName,
     nav,
-    classificationSelect: classificationSelect,
     errors: null,
-    inv_id: itemData[0].inv_id,
-    inv_make: itemData[0].inv_make,
-    inv_model: itemData[0].inv_model,
-    inv_year: itemData[0].inv_year,
-    inv_price: itemData[0].inv_price,
+    inv_id: itemData.inv_id,
+    inv_make: itemData.inv_make,
+    inv_model: itemData.inv_model,
+    inv_year: itemData.inv_year,
+    inv_price: itemData.inv_price,
   })
 }
 
@@ -257,29 +255,19 @@ invCont.buildDeleteInventory = async function (req, res, next) {
  * ************************** */
 invCont.deleteInventory = async function (req, res, next) {
   let nav = await utilities.getNav()
-  const { inv_id } = req.body
+  const inv_id = parseInt(req.body.inv_id)
+  const itemData = await invModel.getVehicleById(inv_id)
+  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
+
    
   const deleteResult = await invModel.deleteInventory(inv_id)
 
   if (deleteResult) {
-    const itemName = deleteResult.inv_make + " " + deleteResult.inv_model
     req.flash("notice", `The ${itemName} was successfully deleted.`)
     res.redirect("/inv/")
   } else {
-    const classificationSelect = await utilities.buildClassificationList(classification_id)
-    const itemName = `${inv_make} ${inv_model}`
     req.flash("notice", "Sorry, the deletion failed.")
-    res.status(501).render("inventory/delete-inventory", {
-      title: "Delete " + itemName,
-      nav,
-      classificationSelect: classificationSelect,
-      errors: null,
-      inv_id,
-      inv_make,
-      inv_model,
-      inv_year,
-      inv_price,
-    })
+    res.redirect("inv/delete/inv_id")   
   }
 }
 
